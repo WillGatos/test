d00c9eb19064f7c268991ad63fb709c5eeaa44fa
willGatos <masferrerw@gmail.com>
2023-01-27 01:59:41 +0100

    whatsApp and Cine Introduction

--- src/pages/Admin/Share.jsx
+++ src/pages/Admin/Share.jsx
@@ -15,10 +15,15 @@ function Share() {
 
     return (
         <div>
-            <div>
+            <a target="_blank" href={`https://wa.me/+58419139?text=${todayCinemas.map(e=> {
+                const startDate = new Date(e.time.continuesEventsStartDay);
+                const endDate = new Date(e.time.continuesEventsEndDay);
+                const eventDay = new Date(e.time.eventsDays)
+                return (`🏛${e.exactDirection.placeToSetEvent}🏛%0a🎞${e.name}🎞%0a🕑${e.time.startHour}%0a📆${e.time.continuesEventsStartDay ? `${startDate.getDate()}/${startDate.getMonth() + 1} - ${endDate.getDate()}/${endDate.getMonth() + 1}` :`${eventDay.getDate()}/${eventDay.getMonth() + 1}`}%0a🧐Revisión%0a${e.reviewLink}%0a%0a`)
+            })}`}>
                 Share WhatsApp
-            </div>
-            {`${todayCinemas.map(e=>(`🎞${e.name}🎞 %0a`))}`}
+            </a>
+            
             <div>
                 {todayCinemas.map(cine => (
                     <Cards
